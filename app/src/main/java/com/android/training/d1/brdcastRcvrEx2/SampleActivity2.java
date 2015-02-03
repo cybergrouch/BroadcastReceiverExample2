23a36dc166a748ab3662890c1848e79b4f1512a8
Lange Pantoja <wpantoja@paypal.com>
2015-02-03 11:39:41 +0800

    Initial commit.

---
+++ app/src/main/java/com/android/training/d1/brdcastRcvrEx2/SampleActivity2.java
@@ -0,0 +1,17 @@
+package com.android.training.d1.brdcastRcvrEx2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SampleActivity2 extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		TextView textView = (TextView) findViewById(R.id.tv);
+		textView.setText("SampleActivity 2");		
+	}
+
+}
